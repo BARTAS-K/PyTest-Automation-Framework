7855beb56379b456d8bc4ecdc75119ba1f9b2888
BARTAS-K <bartas.kruczyk@gmail.com>
2024-01-19 16:37:10 +0100

    added new button function

--- pageObjects/loginPage.py
+++ pageObjects/loginPage.py
@@ -6,6 +6,7 @@ class LoginPage:
     singIn = (By.XPATH, "//*[@id='display-account-login-form-start']/div[7]/button")
     popUp2 = (By.XPATH, "//*[@id='popmake-4393']/button")
     passwordMark = (By.XPATH, "//*[@id='display-account-login-form-start']/div[5]/div")
+    untranslated = (By.XPATH, "//*[@id='post-1375']/div/div[3]/div[2]")
 
     def __init__(self, driver):
         self.driver = driver
@@ -18,3 +19,6 @@ class LoginPage:
 
     def passwordMarkButton(self):
         return self.driver.find_element(*LoginPage.passwordMark)
+
+    def untranslatedLink(self):
+        return self.driver.find_element(*LoginPage.untranslated)
