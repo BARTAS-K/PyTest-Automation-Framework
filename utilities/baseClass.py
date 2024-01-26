8dc8bf6e4a8b931df9aa4daadde70324fb333783
BARTAS-K <bartas.kruczyk@gmail.com>
2024-01-26 23:15:00 +0100

    added page object creation method

--- utilities/baseClass.py
+++ utilities/baseClass.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
+from pageObjects.homePage import HomePage
 
 
 @pytest.mark.usefixtures("setup")
@@ -12,7 +13,6 @@ class BaseClass:
     popUp1 = "//*[@id='popmake-4406']/button"
     popUp2 = "//*[@id='popmake-4393']/button"
 
-
     def popUpClose(self, popUp): #should this be in a different file in utilities?
         button = self.driver.find_element(By.XPATH, popUp)
         action = webdriver.common.action_chains.ActionChains(self.driver)
@@ -20,3 +20,6 @@ class BaseClass:
         action.click()
         action.perform()
 
+    def page(self, page):
+        pageObject = page(self.driver)
+        return pageObject
