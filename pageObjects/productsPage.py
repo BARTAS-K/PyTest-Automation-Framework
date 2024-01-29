d24675a676f022cc08e8ab9038d0ab01680c585d
BARTAS-K <bartas.kruczyk@gmail.com>
2024-01-29 18:38:09 +0100

    added new buttons and methods

--- pageObjects/productsPage.py
+++ pageObjects/productsPage.py
@@ -12,6 +12,8 @@ class ProductsPage:
     addToCart = (By.XPATH, "//*[@id='ec_add_to_cart_27']")
     viewCart = (By.XPATH, "//*[@id='ec_product_page']/div[2]/a")
     professionalSuitImage = (By.XPATH, "//*[@id='ec_product_image_effect_3981370']/a")
+    numberOfResults = (By.XPATH, "//*[@id='ec_product_page']/div[1]/span[1]/a[3]")
+    denimCoat = (By.XPATH, "//*[@id='ec_product_image_effect_4381370']/a")
 
     def __init__(self, driver):
         self.driver = driver
@@ -42,3 +44,9 @@ class ProductsPage:
 
     def professionalSuitImageButton(self):
         return self.driver.find_element(*ProductsPage.professionalSuitImage)
+
+    def numberOfResultsButton(self):
+        return self.driver.find_element(*ProductsPage.numberOfResults)
+
+    def denimCoatImageButton(self):
+        return self.driver.find_element(*ProductsPage.denimCoat)
