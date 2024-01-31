c0017f7047d7333be146f267ee9d3fdae9390b01
BARTAS-K <bartas.kruczyk@gmail.com>
2024-01-31 13:19:20 +0100

    deleted unused methods

--- pageObjects/loginPage.py
+++ pageObjects/loginPage.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 class LoginPage:
 
     singIn = (By.XPATH, "//*[@id='display-account-login-form-start']/div[7]/button")
-    popUp2 = (By.XPATH, "//*[@id='popmake-4393']/button")
     passwordMark = (By.XPATH, "//*[@id='display-account-login-form-start']/div[5]/div")
     untranslated = (By.XPATH, "//*[@id='post-1375']/div/div[3]/div[2]")
 
@@ -13,9 +12,6 @@ class LoginPage:
 
     def singInButton(self):
         return self.driver.find_element(*LoginPage.singIn)
-
-    def popUp2CloseButton(self):
-        return self.driver.find_element(*LoginPage.popUp2)
 
     def passwordMarkButton(self):
         return self.driver.find_element(*LoginPage.passwordMark)
