f1390089086b8917c1d1df77b7e6abd5882d73c9
BARTAS-K <bartas.kruczyk@gmail.com>
2024-01-31 13:22:04 +0100

    deleted unused code

--- utilities/baseClass.py
+++ utilities/baseClass.py
@@ -1,10 +1,7 @@
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-
-from pageObjects.homePage import HomePage
 
 
 @pytest.mark.usefixtures("setup")
@@ -13,7 +10,7 @@ class BaseClass:
     popUp1 = "//*[@id='popmake-4406']/button"
     popUp2 = "//*[@id='popmake-4393']/button"
 
-    def popUpClose(self, popUp): #should this be in a different file in utilities?
+    def popUpClose(self, popUp):
         button = self.driver.find_element(By.XPATH, popUp)
         action = webdriver.common.action_chains.ActionChains(self.driver)
         action.move_to_element_with_offset(button, 50, 50)
