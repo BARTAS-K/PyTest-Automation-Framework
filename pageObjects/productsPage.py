c0f8f6bbad231fb70cdb180826a11be30eccea13
BARTAS-K <bartas.kruczyk@gmail.com>
2024-02-13 21:55:45 +0100

    added logging info messages

--- pageObjects/productsPage.py
+++ pageObjects/productsPage.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.common.by import By
+from utilities.logger import Logger
 
 
-class ProductsPage:
+class ProductsPage(Logger):
 
     shoesImage = (By.XPATH, "//*[@id='ec_product_image_effect_4481370']")
     greyJeansImage = (By.XPATH, "//*[@id='ec_product_image_effect_4281370']/a")
@@ -16,25 +17,33 @@ class ProductsPage:
         self.driver = driver
 
     def shoesImageButton(self):
+        self.getLogger().info("Clicking on \"DNK YELLOW SHOES\" image button")
         return self.driver.find_element(*ProductsPage.shoesImage)
 
     def greyJeansImageButton(self):
+        self.getLogger().info("Clicking on \"DARK GREY JEANS\" image button")
         return self.driver.find_element(*ProductsPage.greyJeansImage)
 
     def blueTshirtButton(self):
+        self.getLogger().info("Clicking on \"BLUE TSHIRT\" image button")
         return self.driver.find_element(*ProductsPage.blueTshirt)
 
     def addToCartButton(self):
+        self.getLogger().info("Clicking on \"ADD TO CART\" button")
         return self.driver.find_element(*ProductsPage.addToCart)
 
     def viewCartButton(self):
+        self.getLogger().info("Clicking on \"VIEW CART\" button")
         return self.driver.find_element(*ProductsPage.viewCart)
 
     def professionalSuitImageButton(self):
+        self.getLogger().info("Clicking on \"PROFESSIONAL SUIT\" image button")
         return self.driver.find_element(*ProductsPage.professionalSuitImage)
 
     def numberOfResultsButton(self):
+        self.getLogger().info("Clicking on different number of results")
         return self.driver.find_element(*ProductsPage.numberOfResults)
 
     def denimCoatImageButton(self):
+        self.getLogger().info("Clicking on \"DENIM COAT\" image button")
         return self.driver.find_element(*ProductsPage.denimCoat)
