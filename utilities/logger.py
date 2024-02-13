7e232bca8db2e05b0da57b849274a56f1d4401e5
BARTAS-K <bartas.kruczyk@gmail.com>
2024-02-13 15:53:38 +0100

    created logger class for logging purposes

---
+++ utilities/logger.py
@@ -0,0 +1,15 @@
+import inspect
+import logging
+
+
+class Logger:
+
+    def getLogger(self):
+        testName = inspect.stack()[1][3]
+        logger = logging.getLogger(testName)
+        fileHandler = logging.FileHandler("logFile.log")
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
+        logger.setLevel(logging.INFO)
+        return logger
